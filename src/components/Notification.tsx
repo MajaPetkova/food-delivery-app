0af579d82c915c67576e801028b8860e2bac3a5f
Maja Petkova <majapetkova1@yahoo.com>
2023-11-09 10:17:12 +0100

    Create components and style for notification component

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-red-500 text-white text-sm md:text-base px-4 flex items-center justify-center text-center'>Free delivery for all orders over $50. Order your food now.</div>
+  ) 
+}
+
+export default Notification
