a8ceb830abf9ec22e89ecb4e3e3a46db88d4737f
Maja Petkova <majapetkova1@yahoo.com>
2023-11-09 11:20:35 +0100

    Add logo and link to the Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Menu from './Menu'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase'>
+      {/* Logo  */}
+      <div className='text-xl'>
+        <Link href="/">Massimo</Link>
+      </div>
+      {/* Menu  */}
+      <Menu/>
+    </div>
   )
 }
 
