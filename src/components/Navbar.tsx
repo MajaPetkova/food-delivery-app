d7d43a52a66e18983c8bd2fd06168d780d3e934b
Maja Petkova <majapetkova1@yahoo.com>
2023-11-10 20:29:26 +0100

    Update style - responsive and update navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,42 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase'>
+    <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24">
+      {/* Left Links  */}
+      <div className="hidden md:flex gap-4 flex:1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
       {/* Logo  */}
-      <div className='text-xl'>
+      <div className="text-xl md:font-bold flex:1 md:text-center">
         <Link href="/">Massimo</Link>
       </div>
-      {/* Menu  */}
-      <Menu/>
+      {/* Mobile Menu  */}
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      {/* Right links  */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex:1">
+        <div className="md:absolute top-3 right-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="phone" width={20} height={20}></Image>
+          <span>01777 888 555</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
