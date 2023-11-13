989613232eefacf0cfb5b4a52a6e3831c14a0d7f
Maja Petkova <majapetkova1@yahoo.com>
2023-11-13 11:24:17 +0100

    Add style and template for Single page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { singleProduct } from "@/data";
+import Image from "next/image";
+import React from "react";
 
 const SingleProductPage = () => {
   return (
-    <div>SingleProductPage</div>
-  )
-}
+    <div className="p-4 lg:p-20 xl:p-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
+      {/* Image Container  */}
+      {singleProduct.img && (<div className="relative">
+        <Image src={singleProduct.img} alt="img" className="object-contain" fill />
+      </div>)}
+      {/* Text Container  */}
+      <div className="">
+       <h1>{singleProduct.title}</h1>
+       <p>{singleProduct.desc}</p>
+      </div>
+    </div>
+  );
+};
 
-export default SingleProductPage
+export default SingleProductPage;
