3c682a6a4559be41fad36abf4991e42f21667300
Maja Petkova <majapetkova1@yahoo.com>
2023-11-16 18:20:55 +0100

    Update style to cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const CartPage = () => {
   return (
-    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500">
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
       {/* Products Container  */}
-      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll">
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:p-20 xl:p-40">
         {/* Single Item Container  */}
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
@@ -37,21 +37,25 @@ const CartPage = () => {
       </div>
       
       {/* Payment Container  */}
-      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center">
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:p-20 xl:p-40 2xl:text-lx 2xl:gap-6">
         <div className="flex justify-between">
           <span>Subtotal (3items)</span>
           <span>$81.70</span>
         </div>
         <div className="flex justify-between">
-          <span>Subtotal (3items)</span>
-          <span>$81.70</span>
+          <span>Service Cost</span>
+          <span>$0</span>
         </div>
         <div className="flex justify-between">
-          <span>Subtotal (3items)</span>
-          <span>$81.70</span>
+          <span>Delivery Cost</span>
+          <span className="text-green-500">FREE</span>
         </div>
-        <hr />
-        <button className="bg-red-500 rounded-md text-white p-3 w-1/2">Checkout</button>
+        <hr className="my-2"/>
+        <div className="flex justify-between">
+          <span>TOTAL(INCL. VAT)</span>
+          <span className="font-bold">$84.00</span>
+        </div>
+        <button className="bg-red-500 rounded-md text-white p-3 w-1/2 uppercase self-end">Checkout</button>
       </div>
     </div>
   );
