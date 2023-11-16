a51ffd1ea7c7f1470cf22a538c0abc15002bfe65
Maja Petkova <majapetkova1@yahoo.com>
2023-11-16 18:51:39 +0100

    Create template for login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="">
+      {/* Box  */}
+      <div className="">
+        {/* Image Container  */}
+        <div className="relative ">
+          <Image src="/loginBg.png" alt="img" fill />
+        </div>
+        {/* Form Container  */}
+        <div className="">
+          <h1>Welcome</h1>
+          <p>Log into your account or create a new one using social buttons</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100">
+            <Image src="/google.png" alt="" width={20} height={20} className="object-contain"/>
+            <span>Sign in with google</span>
+          </button>
+          <button className="flex gap-4 p-4 ring-1 ring-blue-100">
+            <Image src="/facebook.png" alt="" width={20} height={20} className="object-contain"/>
+            <span>Sign in with facebook</span>
+          </button>
+          <p>Have a problem? <Link href="/">Contact us</Link> </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
